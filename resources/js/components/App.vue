fe8336307b091af425ee9d223bba08a0f4f36e6b
Gerardo Calia <sofesso97@gmail.com>
2022-07-06 17:27:59 +0200

    Test Commit front.js webpack

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,11 +2,20 @@
     <div>
         <h1>App Page</h1>
         <div>
-            <div v-for="el,i in apartments" :key="i" class="card ml-2 p-2" style="width: 18rem;">
-                <img class="card-img-top" :src="`${el.cover_image}`" alt="Card image cap">
+            <div
+                v-for="(el, i) in apartments"
+                :key="i"
+                class="card ml-2 p-2"
+                style="width: 18rem"
+            >
+                <img
+                    class="card-img-top"
+                    :src="`${el.cover_image}`"
+                    alt="Card image cap"
+                />
                 <div class="card-body">
-                    <h5 class="card-title">{{el.title}}</h5>
-                    <p class="card-text">{{el.description}}</p>
+                    <h5 class="card-title">{{ el.title }}</h5>
+                    <p class="card-text">{{ el.description }}</p>
                     <a href="#" class="btn btn-primary">Vai ad appartamento</a>
                 </div>
             </div>
@@ -14,28 +23,26 @@
     </div>
 </template>
 
-
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-    export default {
-        name: "App",
-        data(){
-          return {
-             apartments: null
-          }
-        },
+export default {
+    name: "App",
+    data() {
+        return {
+            apartments: null,
+        };
+    },
 
-        mounted() {
-          this.GetApartments()
+    mounted() {
+        this.GetApartments();
+    },
+    methods: {
+        GetApartments() {
+            axios.get("http://127.0.0.1:8000/api/apartments").then((res) => {
+                this.apartments = res.data;
+            });
         },
-        methods: {
-            GetApartments(){
-                axios.get("http://127.0.0.1:8000/api/apartments").then((res) => {this.apartments = res.data})
-            }
-             
-             
-            
-        }
-    }
+    },
+};
 </script>
