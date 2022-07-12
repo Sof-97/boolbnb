e28975a465b59c610f1e40f6566adbe6dbba9611
alexis39tt <alessandro.fonzi@gmail.com>
2022-07-12 19:34:18 +0200

    ho cancellato una cosa

--- resources/views/admin/apartments/sponsor.blade.php
+++ resources/views/admin/apartments/sponsor.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <h1>ID appartamento: {{ $apartment->id }}</h1>
 @foreach ($plans as $plan)
-    <button href="{{ route('admin.apartments.bronze', $apartment->id) }}">Compra il piano {{ $plan->name }} a €{{ $plan->price }}</button>
+    <button">Compra il piano {{ $plan->name }} a €{{ $plan->price }}</button>
 
 @endforeach
 @endsection
