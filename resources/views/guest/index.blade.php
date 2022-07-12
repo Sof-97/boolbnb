a170deb7dc9bd288bc08f1f62954abc27213e804
Gerardo Calia <sofesso97@gmail.com>
2022-07-12 23:06:39 +0200

    Fix merge branch angelo

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -12,122 +12,21 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     {{-- Fontawesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Welcome Page</title>
-    <style>
-        *,
-        *::after,
-        *::before {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        .index-nav-img {
-            padding: 20px;
-            display: flex;
-            align-items: center;
-            font-size: 32px;
-            font-weight: 700
-        }
-
-        .index-nav-img,
-        img {
-            width: 60px;
-            padding-right: 16px
-        }
-
-        .index-title {
-            text-align: center;
-            color: red;
-            font-weight: 700;
-        }
-
-        .index-line {
-            border-bottom: 1px grey solid;
-            width: 80%;
-            margin: 0 auto;
-            margin-bottom: 16px;
-        }
-        .hero{
-        /* background-image: url("../../../public/img/hero.jpg"); */
-        width: fit-content;
-        height: 2%;
-        margin: 0 auto;
-    }
-    </style>
-
 </head>
 
 <body>
-@include('layouts.nav-index')
-<div class="hero">
-    <img src="{{ asset('img/hero.jpg') }}" alt="">
-</div>
-  
-    <header>
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+    @include('layouts.nav-index')
+    <div class="hero">
+        <img src="{{ asset('img/hero.jpg') }}" alt="">
+    </div>
 
-                <div class="navbar-brand" href="{{ url('/') }}">
-                    <img style="height: 50px; width: 50px;" src="{{ asset('img/airbnb-logo.png') }}" alt="tag">
-                    <span class="title" style="color: $color-accent">BoolBnB</span>
-                </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-uppercase"
-                                    href="{{ route('admin.apartments.index') }}" role="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
     <div id="root"></div>
     <script src="{{ asset('js/front.js') }}"></script>
-@include('layouts.footer')
+    @include('layouts.footer')
 </body>
 
 </html>
