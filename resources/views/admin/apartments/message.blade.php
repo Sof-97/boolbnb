e85b187ca9a4159a2dd22078c9713852f0e53215
alexis39tt <alessandro.fonzi@gmail.com>
2022-07-14 01:51:58 +0200

    stampati in pagina solo gli appartamenti che hanno messaggi

--- resources/views/admin/apartments/message.blade.php
+++ resources/views/admin/apartments/message.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.backOffice')
 
 @section('content')
-    <h1>Messaggi</h1>
-    <div class="container">
-        @foreach ($apartments as $apt)
-        @if ($apt->message)
+<h1>Messaggi</h1>
+<div class="container">
+    @foreach ($apartments as $apt)
+    <div class="singleapartment" style="margin-bottom: 70px;">
+        @foreach ($apt->message as $message)
+
+        @if($loop->iteration == 1)
         <h2>Appartamento "{{ $apt->title }}"</h2>
         @endif
-            @foreach ($apt->message as $message)
-                <div style=" padding: 20px; border:1px solid black; border-radius: 20px;margin:20px 0;">
-                    <p>Messagio da: <u>{{ $message->email_sender }}</u></p>
-                    <p>
-                        {{ $message->text }}
-                    </p>
-                </div>
-                @endforeach
-                @endforeach
-            </div>
+        <div style=" padding: 20px; border:1px solid black; border-radius: 20px;margin:20px 0;">
+            <p>Messagio da: <u>{{ $message->email_sender }}</u></p>
+            <p>
+                {{ $message->text }}
+            </p>
+        </div>
+
+        @endforeach
+    </div>
+    @endforeach
+
+</div>
 @endsection
