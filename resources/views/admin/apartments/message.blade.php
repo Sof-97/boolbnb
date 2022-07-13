6522ee407a5bfc8cdd07acc640c60b207f56a3e7
alexis39tt <alessandro.fonzi@gmail.com>
2022-07-14 01:36:49 +0200

    aggiunto elenco messaggi per appartamento

--- resources/views/admin/apartments/message.blade.php
+++ resources/views/admin/apartments/message.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.backOffice')
 
 @section('content')
-    <h2>Message</h2>
+    <h1>Messaggi</h1>
     <div class="container">
-        @foreach ($apartments as $item)
-            @foreach ($item->message as $message)
+        @foreach ($apartments as $apt)
+        @if ($apt->message)
+        <h2>Appartamento "{{ $apt->title }}"</h2>
+        @endif
+            @foreach ($apt->message as $message)
                 <div style=" padding: 20px; border:1px solid black; border-radius: 20px;margin:20px 0;">
-                    <p>Messagio da: {{ $message->email_sender }}</p>
+                    <p>Messagio da: <u>{{ $message->email_sender }}</u></p>
                     <p>
-                        Contenuto:
                         {{ $message->text }}
                     </p>
                 </div>
