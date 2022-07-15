37ee70161fbae2e9555949a1dedfbabcb5fd767c
alexis39tt <alessandro.fonzi@gmail.com>
2022-07-15 12:34:57 +0200

    iniziate stats, modificata pagina messaggi

--- resources/views/admin/apartments/message.blade.php
+++ resources/views/admin/apartments/message.blade.php
@@ -9,10 +9,10 @@
         @foreach ($apt->message as $message)
         <div class="singleapartment-ms">
         @if($loop->iteration == 1)
-        <h2 class="apartment-name-ms">Appartamento: "{{ $apt->title }}"</h2>
+        <h2 class="apartment-name-ms">Appartamento "{{ $apt->title }}"</h2>
         @endif
         <div class="message-ms">
-            <p class="message-sender-ms">Messagio da: <u>{{ $message->email_sender }}</u></p>
+            <p class="message-sender-ms">Messaggio da: <u>{{ $message->email_sender }}</u></p>
             <p class="message-content-ms">
                 {{ $message->text }}
             </p>
--- resources/views/admin/apartments/stats.blade.php
+++ resources/views/admin/apartments/stats.blade.php
@@ -5,12 +5,38 @@
     <h1 class="title-ms">Stats</h1>
     @foreach ($apartments as $apartment)
     <div class="singleapartment-ms">
-    <h2 class="apartment-name-ms">{{ $apartment->title }}</h2>
-            
-        <p>
-            {{ count($apartment->view) }}
-        </p>
+        <h2 class="apartment-name-ms">Appartamento "{{ $apartment->title }}"</h2>
+        <p>Visite totali: {{ count($apartment->view) }}</p>
+        <input type="number" id="input{{ $apartment->id }}" value="{{ count($apartment->view) }}" hidden>
+        <canvas id="myChart" width="400" height="100"></canvas>
+
         @endforeach
     </div>
 </div>
+@endsection
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script defer>
+    const ctx = 'myChart';
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+            datasets: [{
+                label: 'Visite',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: ['#ff385c','white','#ff385c','white','#ff385c','white','#ff385c','white','#ff385c','white','#ff385c','white'],
+                borderColor: ['#ff385c','gray','#ff385c','gray','#ff385c','gray','#ff385c','gray','#ff385c','gray','#ff385c','gray'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
 @endsection
