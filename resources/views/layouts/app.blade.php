f9072d23d212333fde465e72cbbde8beadab7d13
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-07-24 15:01:47 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,44 +2,43 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+				<!-- CSRF Token -->
+				<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+				<title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+				<!-- Scripts -->
+				<script src="{{ secure_asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link href="http://fonts.cdnfonts.com/css/segoe-ui-4" rel="stylesheet">
+				<!-- Fonts -->
+				<link href="http://fonts.cdnfonts.com/css/segoe-ui-4" rel="stylesheet">
 
-    {{-- Fontawesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+				{{-- Fontawesome --}}
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+								integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+								crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- Styles -->
-<<<<<<< HEAD
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-=======
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+				<!-- Styles -->
 
-    <!-- Icona -->
-    <link rel="icon" type="image/x-icon" href="href="{{ asset('icon.jpg') }}">
->>>>>>> c380dbabf8a3bf20e4187ed9292815070ddb0f31
+				<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+
+				<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+				<!-- Icona -->
+				<link rel="icon" type="image/x-icon" href="href="{{ asset('icon.jpg') }}">
 </head>
 
 <body>
-    <div id="app">
-        
+				<div id="app">
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+
+								<main class="py-4">
+												@yield('content')
+								</main>
+				</div>
 </body>
 
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,10 @@
 <div class="sign">
-<<<<<<< HEAD
+
     <div>Write with</div>
     <div class="ft-img"><img src="{{secure_asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
     <div> by Gerry & Friends</div>
-=======
+
     <div>Created with</div>
     <div class="ft-img"><img src="{{asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
     <div>by Team 3</div>
->>>>>>> c380dbabf8a3bf20e4187ed9292815070ddb0f31
 </div>
