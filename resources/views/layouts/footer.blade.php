83d7bdf37109c32fda83c2ff8b8fa8377714fe92
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-07-24 15:03:09 +0200

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,6 @@
 <div class="sign">
 
-    <div>Write with</div>
+    <div>Created with</div>
     <div class="ft-img"><img src="{{secure_asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
     <div> by Gerry & Friends</div>
-
-    <div>Created with</div>
-    <div class="ft-img"><img src="{{asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
-    <div>by Team 3</div>
 </div>
