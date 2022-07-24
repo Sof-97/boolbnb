a9d9f0716e113d91523bfe86d94623bc36b9ccc8
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-07-24 13:55:22 +0200

    secure asset

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="sign">
     <div>Write with</div>
-    <div class="ft-img"><img src="{{seccure_asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
+    <div class="ft-img"><img src="{{secure_asset('img/loveheart.png')}}" style="height: 20px; width: 20px;" alt="Cuore"></div>
     <div> by Gerry & Friends</div>
 </div>
